8945b92842d2ec0f5d374eb561e9d328428bbe72
shuai.li12 <shuai.li12@bianlifeng.com>
2018-10-15 10:39:50 +0800

    feat: 加上图片背景

---
+++ src/components/Home/config.js
@@ -0,0 +1,55 @@
+/**
+ * @file config
+ * @author shuai.li
+ */
+
+export const pageConfig = [
+  {
+    background: "question1.png"
+  },
+  {
+    background: "question2.png"
+  },
+  {
+    background: "question3.png"
+  },
+  {
+    background: "question4.png"
+  },
+  {
+    background: "question5.png"
+  },
+  {
+    background: "question6.png"
+  },
+  {
+    background: "question7.png"
+  },
+  {
+    background: "question8.png"
+  },
+  {
+    background: "question9.png"
+  },
+  {
+    background: "question10.png"
+  },
+  {
+    background: "question11.png"
+  },
+  {
+    background: "question12.png"
+  },
+  {
+    background: "question13.png"
+  },
+  {
+    background: "question13a.png"
+  },
+  {
+    background: "question13b.png"
+  },
+  {
+    background: "question14.png"
+  },
+];
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,15 @@
-let timer = '';
+import { pageConfig } from './config'
+
+const host = `${location.origin}${location.pathname}`.split('#')[0].replace(/index(\d)?\.html/, '');
+let isClick = false;
+let pageConfigIndex = 0;
+let page2Dom = '';
+let page3Dom = '';
 export default {
   data() {
     return {
       wrapperAnimate: false,
       bigAnimate: false,
-      pageNum: 0,
     };
   },
   methods: {
@@ -12,29 +17,54 @@ export default {
       this.$router.push('homePost');
     },
     trigWrap(number) {
-      $(`.page${number}`).addClass('current-page');
+      if (isClick) return;
+      isClick = true;
+      const currentPageConfig = pageConfig[pageConfigIndex];
+      console.log('pageConfigIndex', pageConfigIndex, currentPageConfig, number)
       if (number === 1) {
-        $(`.page2`).show();
+        $(`.page1`).addClass('wrapper-animate');
+        $('#page2-content').css({
+          background: `url('${host}/static/img/${currentPageConfig.background}') no-repeat`,
+          'background-size': 'cover',
+        });
       }
       if (number === 2) {
-        $(`.page3`).show();
-        $(`.page3-front`).addClass('current-page');
+        $('.page2').css({ 'z-index': 10000 }).addClass('wrapper-animate');
+        // $('.page3').show().find('#page3-content').css({
+        //   background: `url('${host}/static/img/${currentPageConfig.background}') no-repeat`,
+        //   'background-size': 'cover',
+        // });
         setTimeout(() => {
-          $(`.page2`).hide().removeClass('current-page').removeClass('wrapper-animate');
+          $(`.page3`).css({ 'z-index': 9999 }).show().find('#page3-content').css({
+            background: `url('${host}/static/img/${currentPageConfig.background}') no-repeat`,
+            'background-size': 'cover',
+          });
+        }, 1000);
+        setTimeout(() => {
+          $(`.page2`).hide().removeClass('wrapper-animate').css({ 'z-index': 0 });
         }, 2000);
       }
       if ( number === 3) {
+        $('.page3').css({ 'z-index': 10000 }).addClass('wrapper-animate');
         setTimeout(() => {
-          $(`.page2`).show();
-          $(`.page2-front`).addClass('current-page');
-          $(`.page3`).hide().removeClass('current-page').removeClass('wrapper-animate');
+          $(`.page2`).css({ 'z-index': 9999 }).show().find('#page2-content').css({
+            background: `url('${host}/static/img/${currentPageConfig.background}') no-repeat`,
+            'background-size': 'cover',
+          });
+        }, 1000);
+        setTimeout(() => {
+          $(`.page3`).hide().removeClass('wrapper-animate').css({ 'z-index': 0 });
         }, 2000);
       }
-      $(`.page${number}`).addClass('wrapper-animate');
-      this.pageNum = number + 1;
-    }
+      setTimeout(() => {
+        isClick = false;
+        pageConfigIndex = pageConfigIndex + 1;
+      }, 2000)
+    },
   },
   mounted() {
+    page2Dom = $('#page2-content');
+    page3Dom = $('#page3-content');
     setTimeout(() => {
       var screenWidth = document.body.clientWidth,
         screenHeight = document.body.clientHeight,
@@ -49,7 +79,10 @@ export default {
       this.wrapperAnimate = true;
       this.bigAnimate = true;
       setTimeout(() => {
-        this.pageNum = 1;
+        // this.pageNum = 1;
+        $('.face-page').css({
+          'z-index': 0,
+        });
         $('.home-text').addClass('text-all-height');
       }, 2000)
     }, 1000)
